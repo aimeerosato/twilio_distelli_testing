073407571d8f5cc45717a6c49d42716416a42e9d
Aimee Rosato <aimee.rosato@gmail.com>
2016-08-08 13:33:58 -0400

    testing post with webhook

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ var express = require('express');
 var app = express();
 var router = express.Router();
 var bodyParser = require('body-parser');
+var twilio = require('twilio');
 
 //Connect to Twilio
 var dotenv = require('dotenv').config();
@@ -18,23 +19,31 @@ app.get('/', function(req, res) {
 	res.send("hello");
 })
 
-router.post('/send', function(req, res) {
-	console.log("inside router /post");
-	console.log("req is ", req.body);
+// router.post('/send', function(req, res) {
+// 	console.log("inside router /post");
+// 	console.log("req is ", req.body);
 
-	client.sms.messages.post({
-    to: '+' + req.body.number,
-    from: process.env.TWILIO_NUMBER,
-    body: req.body.message
-}, function(err, text) {
-    console.log('You sent: '+ text.body);
-    console.log('Current status of this text message is: '+ text.status);
+// 	client.sms.messages.post({
+//     to: '+' + req.body.number,
+//     from: process.env.TWILIO_NUMBER,
+//     body: req.body.message
+// }, function(err, text) {
+//     console.log('You sent: '+ text.body);
+//     console.log('Current status of this text message is: '+ text.status);
+// });
+// 	res.send("You sent a text"); 
+// });
+
+app.post('/send', twilio.webhook({
+    host:'http://twilio-testing-monday-dev.us-east-1.elasticbeanstalk.com/',
+    protocol:'https'
+}), function(request, response) {
+    var twiml = new twilio.TwimlResponse();
+    twiml.message('This HTTP request came from Twilio!');
+    response.send(twiml);
 });
-	res.send("You sent a text");
 
-});
-
-// app.post('/sms', twilio.webhook(), function(request, response) {
+// app.post('/send', twilio.webhook(), function(request, response) {
 //     var twiml = new twilio.TwimlResponse();
 //     twiml.message('This HTTP request came from Twilio!');
 //     response.send(twiml);
