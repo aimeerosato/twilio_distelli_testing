9b49e3f058dbfa7ea4d485d6a0946ae1250e3eff
Aimee Rosato <aimee.rosato@gmail.com>
2016-08-08 17:33:54 -0400

    test with different messages

--- main.js
+++ main.js
@@ -18,25 +18,38 @@ app.use(bodyParser.urlencoded({
 app.get('/', function(req, res) {
 	res.send("hello");
 })
-//twilio.webhook(),
+//twilio.webhook(), took it out - maybe we don't need it because it's configured on twilio dashboard?
+// router.post('/send', function(req, res) {
+// 	console.log("inside router /post");
+// 	console.log("req is ", req.body);
+
+// 	client.sms.messages.post({
+//     to: '+' + req.body.number,
+//     from: process.env.TWILIO_NUMBER,
+//     body: req.body.message
+// }, function(err, text) {
+//     console.log('You sent: '+ text.body);
+//     console.log('Current status of this text message is: '+ text.status);
+// });
+// 	//res.send("You sent a text"); 
+//   var  twim1 = new twilio.TwimlResponse();
+//   twim1.message("This is your response");
+//   res.send(twim1.toString());
+// });
+
 router.post('/send', function(req, res) {
-	console.log("inside router /post");
-	console.log("req is ", req.body);
-
-	client.sms.messages.post({
-    to: '+' + req.body.number,
-    from: process.env.TWILIO_NUMBER,
-    body: req.body.message
-}, function(err, text) {
-    console.log('You sent: '+ text.body);
-    console.log('Current status of this text message is: '+ text.status);
+    var twilio = require('twilio');
+    var twiml = new twilio.TwimlResponse();
+    if (req.body.Body == 'hello') {
+        twiml.message('Hi!');
+    } else if(req.body.Body == 'bye') {
+        twiml.message('Goodbye');
+    } else {
+        twiml.message('No Body param match, Twilio sends this in the request to your server.');
+    }
+    res.writeHead(200, {'Content-Type': 'text/xml'});
+    res.end(twiml.toString());
 });
-	//res.send("You sent a text"); 
-  var  twim1 = new twilio.TwimlResponse();
-  twim1.message("This is your response");
-  res.send(twim1.toString());
-});
-
 
 // var options = {
 //        host:'http://twilio-testing-monday-dev.us-east-1.elasticbeanstalk.com/',
